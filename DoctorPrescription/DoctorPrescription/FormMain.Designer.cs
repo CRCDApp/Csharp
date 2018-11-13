447a57c46e60fe5de1f5c383ae5b0cf90a67cd55
amirshfn <amirhoseinshafieyoun@gmail.com>
2018-11-13 22:20:33 +0100

    edit button image added

--- DoctorPrescription/DoctorPrescription/FormMain.Designer.cs
+++ DoctorPrescription/DoctorPrescription/FormMain.Designer.cs
@@ -32,6 +32,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.signOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.managementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.drugToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.patientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -113,8 +115,6 @@
             this.prescriptionTableAdapter = new DoctorPrescription.DataSet1TableAdapters.PrescriptionTableAdapter();
             this.panelReport = new System.Windows.Forms.Panel();
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.signOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.PanelPatients.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -158,6 +158,20 @@
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
+            // 
+            // signOutToolStripMenuItem
+            // 
+            this.signOutToolStripMenuItem.Name = "signOutToolStripMenuItem";
+            this.signOutToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.signOutToolStripMenuItem.Text = "Sign out";
+            this.signOutToolStripMenuItem.Click += new System.EventHandler(this.SignOutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // managementToolStripMenuItem
             // 
@@ -418,7 +432,7 @@
             // btnEditPatient
             // 
             this.btnEditPatient.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnEditPatient.Image = ((System.Drawing.Image)(resources.GetObject("btnEditPatient.Image")));
+            this.btnEditPatient.Image = global::DoctorPrescription.Properties.Resources.ediiit;
             this.btnEditPatient.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.btnEditPatient.Name = "btnEditPatient";
             this.btnEditPatient.Size = new System.Drawing.Size(23, 22);
@@ -611,7 +625,7 @@
             // btnAddDrug
             // 
             this.btnAddDrug.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnAddDrug.Image = ((System.Drawing.Image)(resources.GetObject("btnAddDrug.Image")));
+            this.btnAddDrug.Image = global::DoctorPrescription.Properties.Resources.ediiit;
             this.btnAddDrug.Name = "btnAddDrug";
             this.btnAddDrug.RightToLeftAutoMirrorImage = true;
             this.btnAddDrug.Size = new System.Drawing.Size(23, 22);
@@ -825,7 +839,7 @@
             // btnPrescriptionAdd
             // 
             this.btnPrescriptionAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnPrescriptionAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnPrescriptionAdd.Image")));
+            this.btnPrescriptionAdd.Image = global::DoctorPrescription.Properties.Resources.ediiit;
             this.btnPrescriptionAdd.Name = "btnPrescriptionAdd";
             this.btnPrescriptionAdd.RightToLeftAutoMirrorImage = true;
             this.btnPrescriptionAdd.Size = new System.Drawing.Size(23, 22);
@@ -883,20 +897,6 @@
             this.crystalReportViewer1.Size = new System.Drawing.Size(200, 100);
             this.crystalReportViewer1.TabIndex = 0;
             this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
-            // 
-            // signOutToolStripMenuItem
-            // 
-            this.signOutToolStripMenuItem.Name = "signOutToolStripMenuItem";
-            this.signOutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.signOutToolStripMenuItem.Text = "Sign out";
-            this.signOutToolStripMenuItem.Click += new System.EventHandler(this.SignOutToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // FormMain
             // 
